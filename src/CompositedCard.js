31be3ba30059895e09720c7ab17013de4de9d21b
Tim Paul <tim.paul@immutable.com>
2019-08-14 15:23:18 +1000

    WIP on web component build out

--- src/CompositedCard.js
+++ src/CompositedCard.js
@@ -1,11 +1,15 @@
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html } from 'lit-element';
+import ResizeObserver from '@juggle/resize-observer';
 
 import {
   loadingTemplate,
-  mythicLayersTemplate,
-  nonMythicLayersTemplate,
+  mythicImageLayersTemplate,
+  nonMythicImageLayersTemplate,
   baseArtworkLayersTemplate,
-} from './templating';
+  textLayersTemplate,
+} from './Templating';
+
+import { getStyles } from './Styles';
 
 const qualities = [
   'plain',
@@ -21,82 +25,56 @@ const qualities = [
   'mythic',
 ];
 
+const ro = new ResizeObserver(entries => {
+  entries.forEach(entry => entry.target.handleResize(entry));
+});
+
+const loadFonts = () => {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.href = 'src/assets/fonts.css';
+  document.head.appendChild(link);
+};
+
 class CompositedCard extends LitElement {
   static get properties() {
     return {
       protoId: { type: Number },
+      inputProtoData: { type: Object },
       useHiResAssets: { type: Boolean },
       resolutionSettings: {
         lowDpi: Number,
         highDpi: Number,
       },
       quality: Number,
-      qualityName: String,
-      protoCardData: {
-        id: Number,
-        mana: Number,
-        rarity: String,
-        type: String,
-        health: {
-          Int64: Number,
-          valid: Boolean,
-        },
-        attack: {
-          Int64: Number,
-          valid: Boolean,
-        },
-        effect: String,
-        name: String,
-      },
     };
   }
 
   static get styles() {
-    return css`
-      :host {
-        display: flex;
-        position: relative;
-        padding-bottom: 140%;
-        /* @NOTE: 140% === 717 / 512 = 1.400 * 100
-        the aspect of these cards is locked to 1:1.40 */
-        width: 100%;
-        background: gold;
-      }
-
-      img {
-        display: block;
-        width: 100%;
-      }
-
-      .card__artwork {
-        position: absolute;
-        width: 64%;
-        right: 15%;
-        top: 12%;
-        transform: translateZ(0);
-      }
-
-      .card__baseLayer,
-      .card__manaLayer,
-      .card__rarityLayer,
-      .card__wreathLayer {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        transform: translateZ(0);
-      }
-    `;
+    return getStyles();
   }
 
   constructor() {
     super();
-    this.useHiResAssets = false;
     this.resolutionSettings = {
       lowDpi: '256',
       highDpi: '512',
     };
+    this.ch = this.offsetHeight * 0.01;
+    this.cw = this.offsetWidth * 0.01;
+    loadFonts();
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    ro.observe(this);
+    this.getViewProtoData();
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    ro.unobserve(this);
   }
 
   fetchProtoData() {
@@ -106,32 +84,53 @@ class CompositedCard extends LitElement {
     );
   }
 
-  connectedCallback() {
-    super.connectedCallback();
+  handleResize(event) {
+    this.ch = event.borderBoxSize.blockSize * 0.01;
+    this.cw = event.borderBoxSize.inlineSize * 0.01;
+    this.requestUpdate();
+  }
+
+  getViewProtoData() {
     if (this.useHiResAssets) {
       this.resolutionSettings = {
         lowDpi: '512',
         highDpi: '1024',
       };
     }
+    this.qualityName = qualities[this.quality];
+    if (this.protoId) {
+      this.getProtoDataFromApi();
+    } else if (this.inputProtoData) {
+      this.getProtoDataFromInput();
+    }
+  }
 
-    this.fetchProtoData().then(data => {
-      this.protoCardData = { ...data };
-      this.qualityName = qualities[this.quality];
-      this.crammedTextMode = data.effect.split(' ').length >= 14;
-      this.isACreatureOrWeapon = RegExp(/creature|weapon/).test(data.type);
+  getProtoDataFromApi() {
+    return this.fetchProtoData().then(data => {
+      const { id, type, attack, health, effect, name, rarity, god } = data;
+      this.protoCardData = {
+        id,
+        type,
+        attack: attack.Int64,
+        health: health.Int64,
+        effect,
+        name,
+        rarity,
+        god,
+      };
       this.loading = false;
+      this.requestUpdate();
     });
   }
 
-  disconnectedCallback() {
-    super.disconnectedCallback();
+  getProtoDataFromInput() {
+    this.protoCardData = { ...this.inputProtoData };
+    this.protoId = this.inputProtoData.id;
+    this.loading = false;
   }
 
   render() {
     const isMythicCard = this.qualityName === 'mythic';
-    // const isMythicCard = true;
-
     return html`
       ${this.loading
         ? loadingTemplate()
@@ -141,12 +140,21 @@ class CompositedCard extends LitElement {
               protoId: this.protoId,
             })}
             ${isMythicCard
-              ? mythicLayersTemplate({
+              ? mythicImageLayersTemplate({
                   type: this.protoCardData.type,
-                  resultionSettings: this.resolutionSettings,
+                  resolutionSettings: this.resolutionSettings,
                   qualityName: this.qualityName,
                 })
-              : nonMythicLayersTemplate({})}
+              : nonMythicImageLayersTemplate({
+                  resolutionSettings: this.resolutionSettings,
+                  qualityName: this.qualityName,
+                  ...this.protoCardData,
+                })}
+            ${textLayersTemplate({
+              ch: this.ch,
+              cw: this.cw,
+              ...this.protoCardData,
+            })}
           `}
     `;
   }
--- src/templating.js
+++ src/templating.js
@@ -1,10 +1,14 @@
 import { html } from 'lit-element';
+import { styleMap } from 'lit-html/directives/style-map';
+import { unsafeHTML } from 'lit-html/directives/unsafe-html';
 
 export const loadingTemplate = () => html`
   loading ...
 `;
 
 export const baseArtworkLayersTemplate = ({ useHiResAssets, protoId }) => {
+  // console.log('!!!!!!!!!!!', useHiResAssets);
+
   return html`
     <picture class="card__artwork">
       <source
@@ -43,65 +47,265 @@ export const baseArtworkLayersTemplate = ({ useHiResAssets, protoId }) => {
   `;
 };
 
-export const mythicLayersTemplate = ({
+export const mythicImageLayersTemplate = ({
   type,
-  resultionSettings,
+  resolutionSettings,
   qualityName,
 }) => {
   return html`
     <picture class="card__baseLayer">
       <source
-        srcset="https://images.godsunchained.com/card-layers/${resultionSettings.highDpi}/creature_hyperion.webp"
+        srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.highDpi}/creature_hyperion.webp"
         media="(-webkit-min-device-pixel-ratio: 2)"
         type="image/webp"
       />
       <source
-        srcset="https://images.godsunchained.com/card-layers/${resultionSettings.highDpi}/creature_hyperion.png"
+        srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.highDpi}/creature_hyperion.png"
         media="(-webkit-min-device-pixel-ratio: 2)"
         type="image/png"
       />
       <source
-        srcset="https://images.godsunchained.com/card-layers/${resultionSettings.lowDpi}/creature_hyperion.webp"
+        srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.lowDpi}/creature_hyperion.webp"
         type="image/webp"
       />
       <source
-        srcset="https://images.godsunchained.com/card-layers/${resultionSettings.lowDpi}/creature_hyperion.png"
+        srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.lowDpi}/creature_hyperion.png"
         type="image/png"
       />
       <img
-        src="https://images.godsunchained.com/card-layers/${resultionSettings.lowDpi}/${type}_${qualityName}.png"
+        src="https://images.godsunchained.com/card-layers/${resolutionSettings.lowDpi}/${type}_${qualityName}.png"
         class="card__baseLayer__img"
       />
     </picture>
 
     <picture class="card__manaLayer">
       <source
-        srcset="https://images.godsunchained.com/card-layers/${resultionSettings.highDpi}/mythic_hyperion.webp"
+        srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.highDpi}/mythic_hyperion.webp"
         media="(-webkit-min-device-pixel-ratio: 2)"
         type="image/webp"
       />
       <source
-        srcset="https://images.godsunchained.com/card-layers/${resultionSettings.highDpi}/mythic_hyperion.png"
+        srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.highDpi}/mythic_hyperion.png"
         media="(-webkit-min-device-pixel-ratio: 2)"
         type="image/png"
       />
       <source
-        srcset="https://images.godsunchained.com/card-layers/${resultionSettings.lowDpi}/mythic_hyperion.webp"
+        srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.lowDpi}/mythic_hyperion.webp"
         type="image/webp"
       />
       <source
-        srcset="https://images.godsunchained.com/card-layers/${resultionSettings.lowDpi}/mythic_hyperion.png"
+        srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.lowDpi}/mythic_hyperion.png"
         type="image/png"
       />
       <img
-        src="https://images.godsunchained.com/card-layers/${resultionSettings.lowDpi}/mythic_hyperion.png"
+        src="https://images.godsunchained.com/card-layers/${resolutionSettings.lowDpi}/mythic_hyperion.png"
         class="card__manaLayer__img"
       />
     </picture>
   `;
 };
 
-export const nonMythicLayersTemplate = () =>
+export const nonMythicImageLayersTemplate = ({
+  type,
+  resolutionSettings,
+  qualityName,
+  rarity,
+  god,
+  set,
+}) =>
   html`
-    non mythic layers!!!
+    <picture class="card__baseLayer">
+      <source
+        srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.highDpi}/${type}_${qualityName}.webp"
+        media="(-webkit-min-device-pixel-ratio: 2)"
+        type="image/webp"
+      />
+      <source
+        srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.highDpi}/${type}_${qualityName}.png"
+        media="(-webkit-min-device-pixel-ratio: 2)"
+        type="image/png"
+      />
+      <source
+        srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.lowDpi}/${type}_${qualityName}.webp"
+        type="image/webp"
+      />
+      <source
+        srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.lowDpi}/${type}_${qualityName}.png"
+        type="image/png"
+      />
+      <img
+        src="https://images.godsunchained.com/card-layers/${resolutionSettings.lowDpi}/${type}_${qualityName}.png"
+        class="card__baseLayer__img"
+      />
+    </picture>
+
+    <picture class="card__manaLayer">
+      <source
+        srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.highDpi}/${god}_${qualityName}.webp"
+        media="(-webkit-min-device-pixel-ratio: 2)"
+        type="image/webp"
+      />
+      <source
+        srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.highDpi}/${god}_${qualityName}.png"
+        media="(-webkit-min-device-pixel-ratio: 2)"
+        type="image/png"
+      />
+      <source
+        srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.lowDpi}/${god}_${qualityName}.webp"
+        type="image/webp"
+      />
+      <source
+        srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.lowDpi}/${god}_${qualityName}.png"
+        type="image/png"
+      />
+      <img
+        src="https://images.godsunchained.com/card-layers/${resolutionSettings.lowDpi}/${god}_${qualityName}.png"
+        class="card__manaLayer__img"
+      />
+    </picture>
+
+    ${rarity !== 'common'
+      ? html`
+          <picture class="card__rarityLayer">
+            <source
+              srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.highDpi}/rarity_${rarity}.webp"
+              media="(-webkit-min-device-pixel-ratio: 2)"
+              type="image/webp"
+            />
+            <source
+              srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.highDpi}/rarity_${rarity}.png"
+              media="(-webkit-min-device-pixel-ratio: 2)"
+              type="image/png"
+            />
+            <source
+              srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.lowDpi}/rarity_${rarity}.webp"
+              type="image/webp"
+            />
+            <img
+              src="https://images.godsunchained.com/card-layers/${resolutionSettings.lowDpi}/rarity_${rarity}.png"
+              class="card__rarityLayer__img"
+            />
+          </picture>
+        `
+      : null}
+    ${rarity === 'legendary'
+      ? html`
+          <picture class="card__wreathLayer">
+            <source
+              srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.highDpi}/wreath_${qualityName}.webp"
+              media="(-webkit-min-device-pixel-ratio: 2)"
+              type="image/webp"
+            />
+            <source
+              srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.highDpi}/wreath_${qualityName}.png"
+              media="(-webkit-min-device-pixel-ratio: 2)"
+              type="image/png"
+            />
+            <source
+              srcset="https://images.godsunchained.com/card-layers/${resolutionSettings.lowDpi}/wreath_${qualityName}.webp"
+              type="image/webp"
+            />
+            <img
+              src="https://images.godsunchained.com/card-layers/${resolutionSettings.lowDpi}/rarity_wreath_${qualityName}.png"
+              class="card__wreathLayer__img"
+            />
+          </picture>
+        `
+      : null}
+    ${set === 'core'
+      ? html`
+          <picture class="card__lockIconLayer" *ngIf="cardProtoData.">
+            <source
+              srcset="https://images.godsunchained.com/card-layers/lock_${qualityName}.webp"
+              type="image/webp"
+            />
+            <img
+              src="https://images.godsunchained.com/card-layers/lock_${qualityName}.png"
+              class="card__lockIconLayer__img"
+            />
+          </picture>
+        `
+      : null}
   `;
+
+export const textLayersTemplate = ({
+  mana,
+  name,
+  effect,
+  attack,
+  health,
+  type,
+  ch,
+  cw,
+}) => {
+  const isACreatureOrWeapon = RegExp(/creature|weapon/).test(type);
+  const crammedTextMode = effect.split(' ').length >= 14;
+  return html`
+    <div
+      class="card__manaText"
+      style=${styleMap({
+        fontSize: `${ch * 10.5}px`,
+        top: `${ch * 5.5}px`,
+        left: `${cw * 9.5}px`,
+        width: `${cw * 19}px`,
+      })}
+    >
+      ${mana}
+    </div>
+    <div
+      class="card__nameText"
+      style=${styleMap({
+        fontSize: `${ch * 4.93}px`,
+        bottom: `${ch * 35}px`,
+        left: `${cw * 12}px`,
+        right: `${cw * 5}px`,
+      })}
+    >
+      ${name}
+    </div>
+
+    <div
+      class="card__descriptionText"
+      style=${styleMap({
+        fontSize: `${crammedTextMode ? ch * 3.8 : ch * 4}px`,
+        lineHeight: crammedTextMode ? 1.1 : 1.3,
+        bottom: `${ch * 7.75}px`,
+        height: `${ch * 22.5}px`,
+        left: `${cw * 20}px`,
+        right: `${cw * 12}px`,
+      })}
+    >
+      <div class="card__descriptionText__inner">
+        ${unsafeHTML(effect)}
+      </div>
+    </div>
+
+    ${isACreatureOrWeapon
+      ? html`
+          <div
+            class="card__attackText"
+            style=${styleMap({
+              fontSize: `${ch * 9.5}px`,
+              bottom: `${ch * 3.5}px`,
+              width: `${cw * 15}px`,
+              left: `${cw * 10.5}px`,
+            })}
+          >
+            ${attack}
+          </div>
+
+          <div
+            class="card__healthText"
+            style=${styleMap({
+              fontSize: `${ch * 9.5}px`,
+              bottom: `${ch * 2.8}px`,
+              width: `${cw * 15}px`,
+              right: `${type === 'weapon' ? cw * 0.8 : cw * 4.5}px`,
+            })}
+          >
+            ${health}
+          </div>
+        `
+      : null}
+  `;
+};
