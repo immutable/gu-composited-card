c893079dd8cdaf5afa195401245d65423fa22b6b
Tim Paul <tim.paul@immutable.com>
2019-09-10 16:03:07 +1000

    adding a webpack build step for gh pages

---
+++ webpack.ghpages.dev.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'ghpages'),
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+  entry: './ghpages/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'ghpages'),
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.css'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'ghpages/index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.woff2$/i,
+        use: [
+          {
+            loader: 'url-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
---
+++ webpack.ghpages.prod.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: './ghpages/index.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'docs'),
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.css'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'ghpages/index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.woff2$/i,
+        use: [
+          {
+            loader: 'url-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
