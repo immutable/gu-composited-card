7756134b49bda44b3d794a48accc091d040c3f71
Tim Paul <tim.paul@immutable.com>
2019-11-20 15:09:51 +1100

    rename story

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -10,7 +10,7 @@ storiesOf('composited-card', module).add(
 
 storiesOf('composited-card', module)
   .addDecorator(withKnobs)
-  .add('add props', () => {
+  .add('interactive demo', () => {
     const proto = number('proto', Math.floor(Math.random() * 99 + 1));
     const qual = number('quality', Math.floor(Math.random() * 4 + 1));
     return `<composited-card protoId="${proto}" quality="${qual}" />`;
